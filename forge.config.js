ff7ef8a75f0bc671ae1f3c502ebe275fa8b310eb
Vishal <vishalpawar048@gmail.com>
2025-11-16 20:40:50 +0530

    window build settings

--- forge.config.js
+++ forge.config.js
@@ -8,21 +8,12 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+      name: '@electron-forge/maker-appx',
+      config: {
+        publisher: 'CN=developmentca',
+       
+      }
+    }
   ],
   plugins: [
     {
